41cdca571ab2fc4cb49a384e482d488e0f2fb612
AR-JUN-G <arjun07091102@gmail.com>
2026-04-11 00:29:40 +0530

    Add the Package.json and Remove the Connection strings

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb://user:0911@ac-ezadzfn-shard-00-00.fjegnnq.mongodb.net:27017,ac-ezadzfn-shard-00-01.fjegnnq.mongodb.net:27017,ac-ezadzfn-shard-00-02.fjegnnq.mongodb.net:27017/DevTinder?ssl=true&replicaSet=atlas-xwl2sk-shard-0&authSource=admin&appName=NodePractice"
-  );
+  await mongoose.connect(process.env.DB_CONNECTION_STRING);
 };
 
 module.exports = connectDB;
